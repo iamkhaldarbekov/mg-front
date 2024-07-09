4d9669970ac73d447bf4b9f6e6944c72269d5a18
Shakhruz Khaldarbekov <shakhruzkhaldarbekov@gmail.com>
2024-07-09 21:39:17 +0500

    fixes

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import routes from './helpers/routes';
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.querySelector("#application")).render(<RouterProvider router={router} />);
